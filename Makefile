aff543b4a56aef51ccba7ac15e53b69b0a44e894
ging-dev <gingdz11012001@gmail.com>
2021-06-19 18:12:30 +0700

    Refactoring

--- Makefile
+++ Makefile
@@ -4,3 +4,9 @@ LDFLAGS=$(shell pkg-config --libs gtk+-3.0 libxfconf-0 libwnck-3.0)
 
 run: main.c
 	$(CC) $(CFLAGS) main.c -o metacity-compatibility $(LDFLAGS)
+
+install:
+	cp metacity-compatibility /usr/local/bin
+
+uninstall:
+	rm /usr/local/bin/metacity-compatibility
--- main.c
+++ main.c
@@ -3,25 +3,19 @@
 #define WNCK_I_KNOW_THIS_IS_UNSTABLE 1
 #include <libwnck/libwnck.h>
 
-/* Gingdev - Google Style Coding */
+/* Gingdev - Symfony Style Coding */
 
-static void cb_xfwm4_channel_property_changed(XfconfChannel *channel,
-                                              const gchar *property_name,
-                                              const GValue *value,
-                                              WnckScreen *screen) {
-  if (g_str_has_prefix(property_name, "/general/") == TRUE) {
-    const gchar *name = &property_name[9];
-    switch (G_VALUE_TYPE(value)) {
-      case G_TYPE_INT:
-        if (!strcmp(name, "workspace_count")) {
-          wnck_screen_change_workspace_count(screen, g_value_get_int(value));
-        }
-        break;
-    }
-  }
+static void cb_xfwm4_channel_property_changed(
+  XfconfChannel *channel,
+  const gchar *property_name,
+  const GValue *value,
+  WnckScreen *screen
+) {
+  wnck_screen_change_workspace_count(screen, g_value_get_int(value));
 }
 
 int main(int argc, char *argv[]) {
+
   if (!xfconf_init(NULL)) {
     g_critical("Xfconf could not be initialized");
     return 0;
@@ -31,10 +25,17 @@ int main(int argc, char *argv[]) {
 
   WnckScreen *screen = wnck_screen_get_default();
 
-  XfconfChannel *xfsettings = xfconf_channel_new("xfwm4");
+  XfconfChannel *xfsettings = xfconf_channel_new_with_property_base(
+    "xfwm4",
+    "/general/workspace_count"
+  );
 
-  g_signal_connect(xfsettings, "property-changed",
-                   G_CALLBACK(cb_xfwm4_channel_property_changed), screen);
+  g_signal_connect(
+    xfsettings,
+    "property-changed",
+    G_CALLBACK(cb_xfwm4_channel_property_changed),
+    screen
+  );
 
   gtk_main();
 
